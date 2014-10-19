8ad556b8677ed6340b729be2ffe0a26b21889e03
Val Ng <avelire@gmail.com>
2014-10-19 18:23:59 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foodmonger::Application.config.secret_key_base = '60d1f8c706f30a674d301b8911a129ed3db15de3413a7b1f6f30618141a4ca1f7072c449e1f0801dfa3fd060d26df9688b8e1ef7547619e38eaa7127e930c3cd'
