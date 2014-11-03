7f55f2399a0aef8001fbae2b4b1f70d10ba5648c
Val Ng <avelire@gmail.com>
2014-11-03 03:49:03 +0000

    Added validations for all fields

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,6 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
-	validates :name, :presence => true
+	validates :name, :presence => true, length: { minimum: 3 }
+	validates :address, :presence => true, length: { minimum: 3, maximum: 200}
+	validates :description, :presence => true, length: { minimum: 2, maximum: 500}
 end
