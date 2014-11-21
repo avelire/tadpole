ec796a5163b1bed95caf957f419be349d663e371
Val Ng <avelire@gmail.com>
2014-11-21 22:11:26 +0000

    Trigger the email when comments get added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,16 +1,16 @@
 Foodmonger::Application.configure do
 
-  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "example.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV['GMAIL_ADDRESS'],
-    password: ENV['GMAIL_PASSWORD']
-  }
+  # config.action_mailer.smtp_settings = {
+  #  address: "smtp.gmail.com",
+  #  port: 587,
+  #  domain: "example.com",
+  #  authentication: "plain",
+  #  enable_starttls_auto: true,
+  #  user_name: ENV['GMAIL_ADDRESS'],
+  #  password: ENV['GMAIL_PASSWORD']
+  #}
 
   # Automated email when users forget password
   config.action_mailer.default_url_options = { :host => 'localhost:3030'}
